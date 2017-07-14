cb9c2559ec5d487077ae29ba26128f3ae1cb8e1a
IlaisStender <kolesnichenko.julie@gmail.com>
2017-07-14 03:12:19 +0300

    bug fix

--- test/server.js
+++
@@ -1,6 +0,0 @@
-let fs = require("fs");
-let browserify = require("browserify");
-browserify("./index.js")
-    .transform("babelify", { presets: ["es2015"] })
-    .bundle()
-    .pipe(fs.createWriteStream("dist/bundle.js"));
